2fd4629b410eeeeff02a48c9aca42baf23c3d53c
donggni0712 <donggni0712@naver.com>
2023-12-03 22:12:31 +0900

    feat(ansible): Make script

--- IaC/ansible/scripts/init.sh
+++ IaC/ansible/scripts/init.sh
@@ -30,3 +30,15 @@ echo "br_netfilter" | sudo tee /etc/modules-load.d/modules.conf
 
 # 설정 적용
 sudo sysctl --system
+
+# key 사용. 권한 설정
+mkdir -p $HOME/.kube
+
+sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config -f
+
+sudo chmod 777 $HOME/.kube/config
+
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# CNI 설치 및 적용 (calico 사용)
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
